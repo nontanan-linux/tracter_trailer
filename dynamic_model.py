4537017c139a1c1612a1f13365e8d403431a09b8
Nontanan Sommat <nontanansommat123@gmail.com>
2026-06-15 19:30:03 +0700

    update dolly and trailer masses to match 12000kg payload

--- dynamic_model.py
+++ dynamic_model.py
@@ -9,8 +9,8 @@ class TractorTrailerDynamicModel:
                  l_f=0.64, l_r=0.64,
                  C_f=30000.0, C_r=40000.0,
                  # Trailer physical parameters
-                 m_d=200.0, I_zd=100.0, l_fd=0.8, l_rd=0.2, C_d=30000.0,
-                 m_t=12000.0, I_zt=16000.0, l_ft=1.0, l_rt=1.5, C_t=40000.0):
+                 m_d=500.0, I_zd=50.0, l_fd=0.8, l_rd=0.2, C_d=30000.0,
+                 m_t=11500.0, I_zt=6000.0, l_ft=1.0, l_rt=1.5, C_t=40000.0):
         """
         Initialize the dynamic model parameters for Tractor + 1 Drawbar Trailer (Dolly + Trailer Body).
         """
